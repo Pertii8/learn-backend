cd9df4de6a452ee1ec5827fea92dccec59e453dd
Valen.perti <valentinoperticarari8@gmail.com>
2024-07-31 23:17:53 -0300

    Agregamos variables de entorno

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //let  significa q puede cambiar la variable
 //const significa q no va acambiar nunca
-
+require('dotenv').config()
 const http = require("http")// './' accede al directorio dnd estas desp podes importarle alguna carpeta d ahi
 
 function requestController(){
@@ -11,7 +11,11 @@ function requestController(){
 //configurar nuestro sv
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+server.listen(PORT, function () {
+    console.log("Aplicacion corriendo en puerto :" + PORT)
+})
+
 //server.listen(4000)
 //const person = {name :  "valen"};
 
