761ec7851e333b49bac241f26aef3ae46b3c1d6b
Valen.perti <valentinoperticarari8@gmail.com>
2024-07-31 23:32:46 -0300

    gpt

--- script.js
+++ script.js
@@ -3,18 +3,19 @@
 require('dotenv').config()
 const http = require("http")// './' accede al directorio dnd estas desp podes importarle alguna carpeta d ahi
 
-function requestController(){
+function requestController(req, res){
     //logica funcion
     console.log("hola paa");
+    res.end("Hello, world");
 }
 
 //configurar nuestro sv
-const server = http.createServer(requestController)
+const server = http.createServer(requestController);
 
 const PORT = process.env.PORT
 server.listen(PORT, function () {
-    console.log("Aplicacion corriendo en puerto : " + PORT)
-})
+    console.log("Aplicacion corriendo en puerto : " + PORT);
+});
 
 //server.listen(4000)
 //const person = {name :  "valen"};
